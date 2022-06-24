1bce3c1b35990e2bd91283e4910f223f8b99cf4b
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-06-24 03:35:48 +0000

    work

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# ICS2O-Assigment-4-CSharp
+# ICS2O-Assigment-5-CSharp
 
-[![GitHub's Super Linter](https://github.com/MT-Aiden/ICS2O-Assigment-4-CSharp/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MT-Aiden/ICS2O-Assigment-4-CSharp/actions)
+[![GitHub's Super Linter](https://github.com/MT-Aiden/ICS2O-Assigment-5-CSharp/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/MT-Aiden/ICS2O-Assigment-5-CSharp/actions)
 
-[![Run on Repl.it](https://repl.it/badge/github/MT-Aiden/ICS2O-Assigment-4-CSharp)](https://repl.it/github/MT-Aiden/ICS2O-Assigment-4-CSharp)
+[![Run on Repl.it](https://repl.it/badge/github/MT-Aiden/ICS2O-Assigment-5-CSharp)](https://repl.it/github/MT-Aiden/ICS2O-Assigment-5-CSharp)
--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 // Aiden McLeod
-// ICS2O-Assignment4-CSharp
-// April 22 2022
+// ICS2O-Assignment5-CSharp
+// April 28 2022
 
 using System;
 
@@ -8,67 +8,31 @@ class Program
 {
     public static void Main(string[] args)
     {
-        string choice;
-        int sizeprice;
-        double toppingprice;
+        string currentAction = "minus";
+        int numberOfIterations;
+        float number = 1;
+        float pi;
 
-        void enterSize()
+        Console.Write("Enter number of iterations: ");
+        numberOfIterations = Convert.ToInt32(Console.ReadLine());
+        pi = (4 / number);
+        while (numberOfIterations > 1)
         {
-            Console.WriteLine("Enter size of pizza:");
-            Console.WriteLine("1 - Large | $6.00");
-            Console.WriteLine("2 - Extra Large | $10.00");
-            choice = Console.ReadLine();
-            if (choice == "1")
+            if (currentAction == "minus")
             {
-                sizeprice = 6;
+                number = number + 2;
+                pi = pi - (4 / number);
+                currentAction = "plus";
             }
-            else if (choice == "2")
+            else if (currentAction == "plus")
             {
-                sizeprice = 10;
+                number = number + 2;
+                pi = pi + (4 / number);
+                currentAction = "minus";
             }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Please enter a valid number.");
-                enterSize();
-            }
-        };
-
-        void enterToppings()
-        {
-            Console.WriteLine("Enter amount of toppings:");
-            Console.WriteLine("1 - One Topping | $1.00");
-            Console.WriteLine("2 - Two Toppings | $1.75");
-            Console.WriteLine("3 - Three Toppings | $2.50");
-            Console.WriteLine("4 - Four Toppings | $3.35");
-            choice = Console.ReadLine();
-            if (choice == "1")
-            {
-                toppingprice = 1;
-            }
-            else if (choice == "2")
-            {
-                toppingprice = 1.75;
-            }
-            else if (choice == "3")
-            {
-                toppingprice = 2.5;
-            }
-            else if (choice == "4")
-            {
-                toppingprice = 3.35;
-            }
-            else
-            {
-                Console.Clear();
-                Console.WriteLine("Please enter a valid number.");
-                enterToppings();
-            }
-        };
-
-        enterSize();
-        enterToppings();
-        Console.WriteLine("\nThe cost of this pizza is $" + ((sizeprice + toppingprice) + ((sizeprice + toppingprice) * 0.13)).ToString("0.00"));
+            numberOfIterations = numberOfIterations - 1;
+        }
+        Console.WriteLine("π is equal to " + (pi).ToString("0.00"));
         Console.WriteLine("\nFinished.");
     }
 }
